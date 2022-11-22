69b5ffec6c822bb94ce4d7568e5e4da384c0eb5c
FloFranz <florian.franz86@gmx.de>
2022-11-22 16:15:45 +0100

    add data augmentation, train and run model with augmentated data

--- src/datagen.py
+++ src/datagen.py
@@ -8,6 +8,7 @@ from sklearn.model_selection import train_test_split
 class CustomImageDataGenerator(tf.keras.utils.Sequence):
     
     def __init__(self, ds, tilesize, sampletype):
+        
         self.ds   = ds
         self.tilesize = tilesize
         self.ylen = self.ds.y.size // self.tilesize
@@ -28,16 +29,42 @@ class CustomImageDataGenerator(tf.keras.utils.Sequence):
         rgb       = np.array([red,green,blue]).transpose(1,2,0)
         forest    = np.array(forest)
         
-        rgb_tiles    = np.array(np.split(rgb, self.xlen,axis=1))
-        target_tiles = np.array(np.split(forest, self.xlen,axis=1))
+        rgb_tiles    = np.array(np.split(rgb, self.xlen, axis=1))
+        target_tiles = np.array(np.split(forest, self.xlen, axis=1))
         
         # Depending on sampletype, return training, validation or test set (complete set)
+        # Data augmentation is included (rotating the images by 90, 180, and 270 degrees)
         if self.sampletype == 'training' or self.sampletype == 'validation':
+            
             rgb_tiles_tr, rgb_tiles_val, target_tiles_tr, target_tiles_val = train_test_split(rgb_tiles, target_tiles, shuffle=True, test_size=0.1, random_state=0)
             
-            if self.sampletype == 'training': return rgb_tiles_tr, target_tiles_tr
-            else:                             return rgb_tiles_val, target_tiles_val
+            if self.sampletype == 'training':
+                
+                rgb_tiles_tr    = np.concatenate((rgb_tiles_tr,
+                                               tf.image.rot90(image=rgb_tiles_tr),
+                                               tf.image.rot90(image=rgb_tiles_tr, k=2),
+                                               tf.image.rot90(image=rgb_tiles_tr, k=3)),
+                                              axis=0)
+                
+                target_tiles_tr = np.concatenate((target_tiles_tr,
+                                                  tf.image.rot90(np.expand_dims(target_tiles_tr, axis=-1))[:,:,:,0],
+                                                  tf.image.rot90(np.expand_dims(target_tiles_tr, axis=-1), k=2)[:,:,:,0],
+                                                  tf.image.rot90(np.expand_dims(target_tiles_tr, axis=-1), k=3)[:,:,:,0]),
+                                                 axis=0)
+                
+                ng              = np.random.RandomState(11)
+                indexes         = ng.permutation(rgb_tiles_tr.shape[0])
+                rgb_tiles_tr    = rgb_tiles_tr[indexes]
+                target_tiles_tr = target_tiles_tr[indexes]
+                
+                return rgb_tiles_tr, target_tiles_tr
+            
+            else:
+                
+                return rgb_tiles_val, target_tiles_val
         
-        if self.sampletype == 'test': return rgb_tiles, target_tiles
+        if self.sampletype == 'test':
+            
+            return rgb_tiles, target_tiles
         
         return None
